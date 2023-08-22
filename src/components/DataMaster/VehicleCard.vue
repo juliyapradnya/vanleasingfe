887f2ffc5f09808a1df57fe2663ad411feafb118
Juliya <70242702+juliyapradnya@users.noreply.github.com>
2023-08-22 09:54:46 +0800

    vc

--- src/components/DataMaster/VehicleCard.vue
+++ src/components/DataMaster/VehicleCard.vue
@@ -1,6 +1,6 @@
 <template>
   <v-main class="list">
-    <h3 class="text-h3 font-weight-medium mb-5">Vehicle Number</h3>
+    <h3 class="text-h3 font-weight-medium mb-5">Vehicle Card</h3>
 
     <v-card>
       <v-card-title>
@@ -460,3 +460,5 @@ export default {
   },
 };
 </script>
+
+
